0c3f218ca908352823eb2f98da3cd16cb8bc4cbb
Amalbejaoui <bejaoui.amal@esprit.tn>
2026-02-18 17:41:40 +0100

    Reactive Form

---
+++ src/app/features/suggestions/suggestion-form/suggestion-form.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-suggestion-form',
+  templateUrl: './suggestion-form.component.html',
+  styleUrls: ['./suggestion-form.component.css']
+})
+export class SuggestionFormComponent {
+
+  suggestionForm!: FormGroup;
+
+  categories: string[] = [
+    'Infrastructure et bâtiments',
+    'Technologie et services numériques',
+    'Restauration et cafétéria',
+    'Hygiène et environnement',
+    'Transport et mobilité',
+    'Activités et événements',
+    'Sécurité',
+    'Communication interne',
+    'Accessibilité',
+    'Autre'
+  ];
+
+  constructor(private fb: FormBuilder, private router: Router) {
+
+    this.suggestionForm = this.fb.group({
+      title: ['', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.pattern('^[A-Z][a-zA-Z]*$')
+      ]],
+
+      description: ['', [
+        Validators.required,
+        Validators.minLength(30)
+      ]],
+
+      category: ['', Validators.required],
+
+      date: [{ value: new Date(), disabled: true }],
+
+      status: [{ value: 'en_attente', disabled: true }]
+    });
+  }
+
+  onSubmit() {
+    if (this.suggestionForm.valid) {
+
+      const newSuggestion = {
+        id: Math.floor(Math.random() * 1000),
+        ...this.suggestionForm.getRawValue(),
+        nbLikes: 0
+      };
+
+      console.log(newSuggestion);
+
+      this.router.navigate(['/suggestions']);
+    }
+  }
+
+}
